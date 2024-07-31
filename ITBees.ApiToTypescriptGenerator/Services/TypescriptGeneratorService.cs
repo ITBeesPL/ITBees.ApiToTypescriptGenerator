d32eaa7c03ccf46a562c1bbe297a2e75c4cdcb2a
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-31 12:02:34 +0200

    cleanup

--- ITBees.ApiToTypescriptGenerator/Services/TypescriptGeneratorService.cs
+++ ITBees.ApiToTypescriptGenerator/Services/TypescriptGeneratorService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO.Compression;
+﻿using System.IO.Compression;
 using System.Reflection;
 using System.Text;
 using ITBees.ApiToTypescriptGenerator.Interfaces;
